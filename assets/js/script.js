31101ba23bffe5a287bcb0b329a3877e8fac7cbc
Sebastián Fretes <sebafretesca@gmail.com>
2026-03-20 14:29:43 -0300

    remove broken filter select JS causing navigation to crash

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,26 +47,6 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 modalCloseBtn.addEventListener('click', testimonialsModalFunc);
 overlay.addEventListener('click', testimonialsModalFunc);
 
-// custom select variables
-const select = document.querySelector('[data-select]');
-const selectItems = document.querySelectorAll('[data-select-item]');
-const selectValue = document.querySelector('[data-selecct-value]');
-const filterBtn = document.querySelectorAll('[data-filter-btn]');
-
-select.addEventListener('click', function () {
-  elementToggleFunc(this);
-});
-
-// add event in all select items
-for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener('click', function () {
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
-  });
-}
-
 // filter variables
 const filterItems = document.querySelectorAll('[data-filter-item]');
 
@@ -81,21 +61,6 @@ const filterFunc = function (selectedValue) {
     }
   }
 };
-
-// add event in all filter button items for large screen
-let lastClickedBtn = filterBtn[0];
-
-for (let i = 0; i < filterBtn.length; i++) {
-  filterBtn[i].addEventListener('click', function () {
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
-
-    lastClickedBtn.classList.remove('active');
-    this.classList.add('active');
-    lastClickedBtn = this;
-  });
-}
 
 // contact form variables
 const form = document.querySelector('[data-form]');
